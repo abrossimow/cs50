43e4204e19f19382fa4b81bb46f1291518650fd5
ximossi <116030901+abrossimow@users.noreply.github.com>
2025-06-04 18:54:33 +0200

    fixed validation bug from not doing the proper math

--- c_tutorial/credit/credit.c
+++ c_tutorial/credit/credit.c
@@ -30,7 +30,7 @@ int main(void)
         }
     }
     while (!userCreditCardNumberFirstCheck);
-    printf("Your number is: %li \n", userCreditCardNumber);
+    // printf("Your number is: %li \n", userCreditCardNumber);
     if (validate())
     {
         printf("This seems to be legit.\n");
@@ -65,7 +65,7 @@ int main(void)
 bool validate(void)
 {
     long cacheNumber = userCreditCardNumber;
-    int sumEven = 0, sumOdd = 0, counterDigits = 0;
+    int sumEven = 0, sumOdd = 0, counterDigits = 0, cacheOdd = 0;
     char gateOddEven = 'e';
     while (cacheNumber > 0)
     {
@@ -79,23 +79,34 @@ bool validate(void)
                 break;
 
             case 'o':
-                sumOdd += (cacheNumber % 10) * 2;
+                cacheOdd = cacheNumber % 10;
                 cacheNumber /= 10;
+                cacheOdd *= 2;
+                if (cacheOdd > 10)
+                {
+                    sumOdd += cacheOdd % 10;
+                    cacheOdd /= 10;
+                    sumOdd += cacheOdd;
+                }
+                else
+                {
+                    sumOdd += cacheOdd;
+                }
                 counterDigits++;
                 gateOddEven = 'e';
                 break;
         }
     }
-    printf("Cached: sumEven %i, sumOdd %i\n", sumEven, sumOdd);
+    // printf("Cached: sumEven %i, sumOdd %i\n", sumEven, sumOdd);
     int sumValidation = sumOdd + sumEven;
     if (sumValidation % 10 == 0)
     {
-        printf("validation hash: %i\n", sumValidation);
+        // printf("validation hash: %i\n", sumValidation);
         return(true);
     }
     else
     {
-        printf("validation hash: %i\n", sumValidation);
+        // printf("validation hash: %i\n", sumValidation);
         return(false);
     }
 }
@@ -103,5 +114,7 @@ bool validate(void)
 int checkBrand(void)
 {
     int ccnumber = userCreditCardNumber;
+    //loop through ccnumber, safe remainder to tempNumber until ccnumber <100
+    //look at tempNumber and work through defined states
     return(0);
 }
