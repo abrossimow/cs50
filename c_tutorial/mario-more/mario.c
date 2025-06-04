52378e2fce23101531aef0229c979ff87a166b1b
ximossi <116030901+abrossimow@users.noreply.github.com>
2025-06-04 10:04:54 +0200

    additional work

--- c_tutorial/mario-more/mario.c
+++ c_tutorial/mario-more/mario.c
@@ -13,13 +13,19 @@ int main(void) {
 
 int askHeight(void) {
     int i;
+    int max = 8;
     do {
         i = get_int("How high shall the pyramid be? ");
-        if (i > 12) {
-            printf("That would be too big. Maximal size is 12.\nAgain: ");
-        };
+        if (i > max) {
+            printf("That would be too big. Maximal size is %i.\nAgain: ", max);
+        }
+        else if (i <= 0) {
+            printf("That is too small. It should be at least 1, right? \nAgain: ");
+        }
+        // Here, an additional, friendly prompt to use nubmers would be great but get_int() takes care of that internally.
+        // I don't know how to get a return value from that...or if it is possible as such.
     }
-    while (i < 0 || i > 12);
+    while (i == 0 || i <= 0 || i > max);
     return(i);
 }
 
