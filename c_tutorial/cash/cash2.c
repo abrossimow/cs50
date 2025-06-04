d2ee0b1ace1230beb59f968002beb89949a7ddcd
ximossi <116030901+abrossimow@users.noreply.github.com>
2025-06-04 11:48:22 +0200

    One more solution

---
+++ c_tutorial/cash/cash2.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int checkAgainstCoin(int coinValue, int changeLeft);
+
+int main(void) {
+    int coin1 = 25;
+    int coin2 = 10;
+    int coin3 = 5;
+    int coin4 = 1;
+    int coinTotal = 0;
+
+    int changeLeft = get_int("How much CHANGE you got? ");
+    while (changeLeft > 0) {
+
+        int quartersNeeded = checkAgainstCoin(coin1, changeLeft);
+        changeLeft -= quartersNeeded*coin1;
+        printf("Quarters: %i \n", quartersNeeded);
+
+        int dimesNeeded = checkAgainstCoin(coin2, changeLeft);
+        changeLeft -= dimesNeeded*coin2;
+        printf("Dimes: %i \n", dimesNeeded);
+
+        int nickelsNeeded = checkAgainstCoin(coin3, changeLeft);
+        changeLeft -= nickelsNeeded*coin3;
+        printf("Nickels: %i \n", nickelsNeeded);
+
+        int penniessNeeded = checkAgainstCoin(coin4, changeLeft);
+        changeLeft -= penniessNeeded*coin4;
+        printf("Pennies: %i \n", penniessNeeded);
+        
+        coinTotal = quartersNeeded + dimesNeeded + nickelsNeeded + penniessNeeded;
+    }
+    printf("You will need %i coins in total.\n", coinTotal);
+}
+
+int checkAgainstCoin(int coinValue, int changeLeft) {
+    int coinsReturn = 0;
+    while (changeLeft > 0) {
+        if (changeLeft >= coinValue) {
+            changeLeft -= coinValue;
+            coinsReturn++;
+        }
+        else {
+            break;
+        }
+    }
+    return(coinsReturn);
+}
---
+++ c_tutorial/cash/cash3.c
@@ -0,0 +1,40 @@
+#include <cs50.h>
+#include <stdio.h>
+
+//this time, let's do division!
+
+int main(void) {
+    int valueCoin1 = 25;
+    int valueCoin2 = 10;
+    int valueCoin3 = 5;
+    int valueCoin4 = 1;
+
+    int neededCoin1 = 0;
+    int neededCoin2 = 0;
+    int neededCoin3 = 0;
+    int neededCoin4 = 0;
+    
+    int changeLeft = get_int("How much CHANGE you got? ");
+    printf("You will need:\n");
+
+    while (changeLeft > 0) {
+        neededCoin1 = changeLeft / valueCoin1;
+        changeLeft %= valueCoin1;
+        printf("Quarters: %i\n", neededCoin1);
+
+        neededCoin2 = changeLeft / valueCoin2;
+        changeLeft %= valueCoin2;
+        printf("Dimes: %i\n", neededCoin2);
+        
+        neededCoin3 = changeLeft / valueCoin3;
+        changeLeft %= valueCoin3;
+        printf("Nickels: %i\n", neededCoin3);
+
+        neededCoin4 = changeLeft / valueCoin4;
+        changeLeft %= valueCoin4;
+        printf("Pennies: %i\n", neededCoin4);
+    }
+
+    int neededCoinTotal = neededCoin1 + neededCoin2 + neededCoin3 + neededCoin4;
+    printf("Which makes %i in total.\n", neededCoinTotal);
+}
